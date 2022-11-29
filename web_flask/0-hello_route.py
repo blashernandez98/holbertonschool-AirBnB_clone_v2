f556bb25452202a3ec06b366a1ff13e4ec6988ef
blas <blashernandez98@gmail.com>
2022-11-29 16:04:05 -0300

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""" Task 0 module """
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def home():
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
