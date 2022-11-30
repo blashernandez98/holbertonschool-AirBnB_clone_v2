11425b171f8a057e861a6c4f44a37801ecf83303
blas <blashernandez98@gmail.com>
2022-11-29 23:12:59 -0300

    Task3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Task 3 module """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ Home route """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB route """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def clang(text):
+    """ C route """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/c/<text>')
+def python(text="is cool"):
+    """ Python route """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
